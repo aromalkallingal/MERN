ddd4bf34af57abab83e712e6691de80dfb53e1d5
aromalkallingal5@gmail.com <aromalkallingal5@gmail.com>
2025-06-18 23:22:33 +0530

    added things

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,19 @@ import express from "express"
 const app = express()
 
 app.get("/api/notes", (req, res) =>  {
-    res.status(200).send("you got 10 notes"); 
+    res.status(200).send("you got 20 notes");
+});
+
+app.post("./api/notes", (req,res) => {
+    res.status(201).json({message:"post created successfully!"})
 })
+
+app.put("./api/notes/:id", (req,res) => {
+    res.status(200).json({message:"post updated successfully!"})
+})
+
+http://localhost:5001/api/notes/212132
+
 
 app.listen(5001, () => {
     console.log("Server started on PORT: 5001")
