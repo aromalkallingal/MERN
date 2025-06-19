76944ea16bf7ce8c0b252022dc0668552ad2f55f
aromalkallingal5@gmail.com <aromalkallingal5@gmail.com>
2025-06-19 22:12:43 +0530

    test

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,10 @@ app.put("./api/notes/:id", (req,res) => {
     res.status(200).json({message:"post updated successfully!"})
 })
 
+app.put("./api/notes/:id", (req,res) => {
+    res.status(202).json({message:"post deleted successfully!"})
+})
+
 http://localhost:5001/api/notes/212132
 
 
